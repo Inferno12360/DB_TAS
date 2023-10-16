cb811568a920eace3fcd087bbcc8572dade1696d
Schoellmaster <143408799+Schoellmaster@users.noreply.github.com>
2023-10-16 14:07:30 +0200

    Update Anmeldeformular.php

--- Anmeldeformular.php
+++ Anmeldeformular.php
@@ -75,7 +75,6 @@
                 <label for="vorname">Vorname:</label>
                 <input type="text" id="vorname" name="vorname" required>
             </div>
-            <p>huansohn</p>
             <div class="form-group">
                 <label for="nachname">Nachname:</label>
                 <input type="text" id="nachname" name="nachname" required>
